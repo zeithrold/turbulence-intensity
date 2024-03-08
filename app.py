cff3c87f91a910304186dd58291e020aeb8bba88
Zeithrold <zeithrold@gmail.com>
2024-03-08 01:24:47 +0000

    Added demo code and dependencies

---
+++ app.py
@@ -0,0 +1,9 @@
+# Demo code from huggingface
+
+import gradio as gr
+
+def greet(name):
+    return "Hello " + name + "!!"
+
+iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+iface.launch()
